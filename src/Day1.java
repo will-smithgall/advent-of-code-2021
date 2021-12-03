3d7881130f16dd6bc896211c72f3d713929010e2
will <was51@pitt.edu>
2021-12-02 22:29:04 -0500

    added day 1 and day 2

---
+++ src/Day1.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Day1 {
+
+    public static ArrayList<Integer> sumByThree(ArrayList<Integer> ints) {
+        ArrayList<Integer> threes = new ArrayList<Integer>();
+
+        for (int i = 2; i < ints.size(); i++) {
+            threes.add(ints.get(i - 2) + ints.get(i - 1) + ints.get(i));
+        }
+
+        return threes;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("inputs/input_day_one.txt"));
+        ArrayList<Integer> ints = new ArrayList<Integer>();
+        int counter = 0;
+
+        String line = br.readLine();
+        while (line != null) {
+            ints.add(Integer.parseInt(line));
+            line = br.readLine();
+        }
+
+        for (int i = 1; i < ints.size(); i++) {
+            if (ints.get(i) > ints.get(i - 1)) {
+                counter++;
+            }
+        }
+
+        ArrayList<Integer> threes = sumByThree(ints);
+        int counterTwo = 0;
+
+        for (int i = 1; i < threes.size(); i++) {
+            if (threes.get(i) > threes.get(i - 1)) {
+                counterTwo++;
+            }
+        }
+
+        System.out.println(counterTwo);
+    }
+}
---
+++ src/Day2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("inputs/day2.txt"));
+        String line = br.readLine();
+
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+
+        while (line != null) {
+            String[] terms = line.split(" ");
+
+            if (terms[0].equals("forward")) {
+                horizontal = horizontal + Integer.parseInt(terms[1]);
+                depth = depth + (aim * Integer.parseInt(terms[1]));
+            } else if (terms[0].equals("down")) {
+                // depth = depth + Integer.parseInt(terms[1]);
+                aim = aim + Integer.parseInt(terms[1]);
+            } else {
+                // depth = depth - Integer.parseInt(terms[1]);
+                aim = aim - Integer.parseInt(terms[1]);
+            }
+
+            line = br.readLine();
+        }
+
+        System.out.println(horizontal * depth);
+
+    }
+}

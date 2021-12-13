b227fe7091ce3d5d7eb53fc98cd59cd592c80b9b
will <was51@pitt.edu>
2021-12-13 15:49:09 -0500

    finished day 6

--- src/Day6.java
+++ src/Day6.java
@@ -30,20 +30,45 @@ public class Day6 {
         return fish.size();
     }
 
-    public static int getFishPartTwo(ArrayList<Integer> fish) {
-        return 0;
-    }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("inputs/test.txt"));
-        ArrayList<Integer> fish = new ArrayList<Integer>();
+        BufferedReader br = new BufferedReader(new FileReader("inputs/day6.txt"));
+        ArrayList<Integer> fishPartOne = new ArrayList<Integer>();
 
         String[] line = br.readLine().split(",");
 
         for (int i = 0; i < line.length; i++) {
-            fish.add(Integer.parseInt(line[i]));
+            fishPartOne.add(Integer.parseInt(line[i]));
         }
 
-        System.out.println(numFishPartOne(fish));
+        //int doesn't have enough storage
+        long[] fish = new long[10];
+        long totalFish = 0;
+
+        for (String s : line) {
+            fish[Integer.parseInt(s)]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            //number of fish restarting their cycle (day 6) is the number of fish at 0 from the previous day
+            fish[7] += fish[0];
+
+            //number of fish creating new fish (day 8 fish) is the number of fish at 0 from previous day
+            fish[9] = fish[0];
+
+            for (int j = 0; j < 9; j++) {
+                //increment day 0 fish amount by day 1 fish amount
+                fish[j] = fish[j+1];
+            }
+
+            //reset day 8
+            fish[9] = 0;
+        }
+
+        for (int i = 0; i < fish.length; i++) {
+            totalFish += fish[i];
+        }
+
+//        System.out.println(numFishPartOne(fishPartOne));
+        System.out.println(totalFish);
     }
 }
